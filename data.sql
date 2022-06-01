7cd54ef3a01b43bd30929148b88a8403a7d26697
Munezero Christian <chamsf5@aol.com>
2022-06-01 15:25:36 +0200

    Inserted data into owners and species tables

--- data.sql
+++ data.sql
@@ -100,3 +100,10 @@ values (
 	22
 );
 
+INSERT INTO owners(full_name, age)
+values ('Sam Smith',34),('Jennifer Orwell',19),
+('Bob',45),('Melody Pond',77),
+('Dean Winchester',14),('Jodie Whittaker',38);
+
+INSERT INTO species(name)
+values ('Pokemon'),('Digimon');
